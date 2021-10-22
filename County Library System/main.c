74ad505f4c250081e0c6beb1651eb1451f7c9866
purity66 <90901777+purity66@users.noreply.github.com>
2021-10-22 19:46:40 +0300

    Add files via upload

---
+++ County Library System/main.c
@@ -0,0 +1,31 @@
+/*
+Project:County Library System
+Author:Purity
+Date:Oct,2021
+Compiler:GNU GCC
+Language:C99
+License:MIT
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("County Library\n");
+    printf("Welcome Purity\n");
+    printf("What would you like to do\n");
+    printf("1.Add a new user\n");
+    printf("2.Get a new book");
+    printf("3.Add book information\n");
+    printf("4.Display book information\n");
+    printf("5.List all books of given author\n");
+    printf("6.List the tittle of specified book\n");
+    printf("7.Search books\n");
+    printf("8.View books\n");
+    printf("9.List the count of books in the library\n");
+    printf("10.Exit\n");
+    return 0;
+
+}
